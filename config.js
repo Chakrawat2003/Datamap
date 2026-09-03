ded235edc022ecaf76363d3751f3f420919b0e16
Chakrawat2003 <gungan2011ca@gmail.com>
2026-09-03 16:56:48 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://cplzytrrebmkqkqkthpr.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_Cn8JRiTxgObV3mQDggJG7Q__m2DPIuO",
+  ESRI_API_KEY: "" // หากไม่มี API Key ของ Esri สามารถปล่อยว่างไว้แบบนี้ได้เลย (โค้ด app.js จะปรับ URL ให้ใช้งาน Esri Imagery ได้อัตโนมัติ)
+};
